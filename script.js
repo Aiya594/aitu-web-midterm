89eaac6b8c35e572a3b31ad76ee610a73f06ab8e
aiya <aichkaxxx2007@gmail.com>
2025-11-05 10:27:21 +0500

    form validation with jquery

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    // --- Page fade-in animation ---
+    //Page fade-in animation
     $("body").css("display", "none").fadeIn(1000);
 
-    // --- Animate buttons on hover ---
+    //Animate buttons on hover 
     $(".btn").hover(
         function () {
             $(this).animate({ paddingLeft: "25px" }, 200);
@@ -12,14 +12,10 @@ $(document).ready(function () {
         }
     );
 
-    // --- Highlight active navigation link ---
+    //Highlight active navigation link 
     $(".nav-list a").on("click", function () {
         $(".nav-list a").removeClass("active");
         $(this).addClass("active");
     });
-
- 
-
-
 
 });
